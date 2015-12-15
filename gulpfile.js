be5152e11846e8a47675596dfd065345e0690ba2
Helder <helder@lamoulade.com>
2015-12-15 18:20:00 +0000

    browserify added to the setup

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,13 @@ var cssnext = require('cssnext');
 var precss = require('precss');
 var mixins = require("postcss-mixins");
 var browserSync = require('browser-sync').create();
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
 
 gulp.task('css', function () {
 	var processors = [autoprefixer, cssnext, precss];
 
-	return gulp.src('./src/*.css')
+	return gulp.src('./src/main.css')
 		.pipe(postcss(processors))
 		.pipe(gulp.dest('./dest'))
 		.pipe(browserSync.stream());
@@ -20,9 +22,11 @@ gulp.task('reload', function () {
 });
 
 gulp.task('watch', function () {
+	gulp.watch('./src/*.js', ['browserify']);
 	gulp.watch('./src/*.css', ['css']);
 	gulp.watch("./dest/*css", ['reload']);
 	gulp.watch("index.html", ['reload']);
+	gulp.watch("./dest/*js", ['reload']);
 });
 
 gulp.task('serve', ['watch'], function () {
@@ -36,3 +40,10 @@ gulp.task('serve', ['watch'], function () {
 });
 
 gulp.task('default', ['serve']);
+
+gulp.task('browserify', function () {
+	return browserify('./src/app.js')
+			.bundle()
+			.pipe(source('bundle.js'))
+			.pipe(gulp.dest('./dest/'));
+});
---
+++ src/app.js
@@ -0,0 +1,4 @@
+var module1 = require('./module1.js');
+
+console.log(module1);
+console.log('foobar 1234!');
